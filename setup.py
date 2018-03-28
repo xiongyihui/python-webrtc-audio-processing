60875103e960ca86acc8e18b53abd613db2389c5
Yihui Xiong <yihui.xiong@hotmail.com>
2018-03-28 16:59:32 +0800

    0.0.2

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ swig_opts = (
 
 setup(
     name='webrtc_audio_processing',
-    version='0.0.1',
+    version='0.0.2',
     description='Python bindings of webrtc audio processing',
     long_description=__doc__,
     author='Yihui Xiong',
